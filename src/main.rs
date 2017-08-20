644c22b584b4f42437337035143bc6e7cca06bd4
Arthur Mendes <arthurmco@gmail.com>
2017-08-20 18:55:52 -0300

    Add a basic shell interface

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,51 @@
 mod picture;
 
-fn main() {
-    let img = picture::Image::create_empty("Test", 256, 256);
+use std::io;
+use std::io::Write;
+use picture::*;
 
-    println!("{0} has {1}x{2} pixels", img.name, img.get_width(), img.get_height());
+fn draw_command_line(ps1: &str) -> Option<String> {
+    print!("{}", ps1);
+    io::stdout().flush().expect("Error while flushing stdout");
+
+    let mut command = String::new();
+    if io::stdin().read_line(&mut command).is_err() {
+        return None;
+    };
+
+    command = String::from(command.trim());
+
+    if command.len() == 0 {
+        return None;
+    }
+
+    return Some(command);
+}
+
+fn main() {
+    println!("oxImage v0.1");
+    println!("Copyright (c) 2017 Arthur M");
+    println!("Licensed under MIT License\n");
+
+    let root = ImageCollectionList::create("root");
+    let cwd = root;
+    loop {
+
+        let cmd = match draw_command_line("> ") {
+            Some(command) => command,
+            None => continue
+        };
+
+        if cmd == "quit" {
+            println!("Exiting...\n");
+            break;
+        } else if cmd == "pwd" {
+            println!(" {}/ \n", cwd.get_display_name());
+        } else {
+            println!(" Command {} does not exist\n", cmd);
+        }
+
+    }
+
+
 }
